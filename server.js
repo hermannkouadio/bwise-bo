ebb78f3271e8c793ef35faea3aa7d477f8149e59
Hermann KOUADIO <kouadiohermann6@gmail.com>
2020-06-10 14:18:34 +0000

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Bwise-bo'));
+app.use(express.static(__dirname + '/dist/bwise-bo'));
 
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/Bwise-bo/index.html'));
+res.sendFile(path.join(__dirname+'/dist/bwise-bo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
